359f1aa19bd3e107a4c79ad8bbf708ea811f8143
Ratchanon Panmas <28267945+ratchanonp@users.noreply.github.com>
2024-10-29 19:20:35 +0700

    Refactor type hinting in Two Sum solution

--- 1. Two Sum/Solution.py
+++ 1. Two Sum/Solution.py
@@ -1,5 +1,7 @@
+from typing import List
+
 class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         
         store = {}
 
