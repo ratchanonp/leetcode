dbfbf4e48c252ad773bc8b0bb9353077f5bc8705
Ratchanon Panmas <28267945+ratchanonp@users.noreply.github.com>
2024-10-30 22:48:00 +0700

    Add solution for Palindrome Number problem

---
+++ 9. Palindrome Number/Solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+
+        str_x = str(x)
+        
+        return str_x == str_x[::-1]
+
+
+s = Solution()
+
+print(s.isPalindrome(0))
